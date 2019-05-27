748a663c4247af30fd6d99e68a1d272cd0fd5af8
AVGasanov <avgasanov@gmail.com>
2019-05-27 23:57:49 +0400

    Add Firebase to Project

--- socketIO101/chat.js
+++ socketIO101/chat.js
@@ -8,12 +8,23 @@ const expressServer = app.listen(9000);
 const io = socketio(expressServer);
 io.on('connection',(socket)=>{
     socket.emit('messageFromServer',{data:"Welcome to the socketio server"});
-    socket.on('messageToServer',(dataFromClient)=>{
+    socket.on('connect', () => {
+        console.log('someone connected')
+    })
+    socket.on('TIME_TO_SERVER',(dataFromClient)=>{
+        console.log(dataFromClient)
+    })
+    socket.on('TEST_TO_SERVER', (dataFromClient) => {
+        console.log('TEST recived')
         console.log(dataFromClient)
     })
     socket.on('newMessageToServer',(msg)=>{
-        // console.log(msg)
+        console.log(msg)
         io.emit('messageToClients',{text:msg.text})
+    })
+    socket.on('ACK_TIME_TO_SERVER', (msg, callback) => {
+        console.log(msg);
+        callback();
     })
 })
 
